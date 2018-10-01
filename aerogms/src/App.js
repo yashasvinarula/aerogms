7d5522e18b12960587804f0582ecc9df8fc3bc15
Parveen Sahrawat <parveen.sahrawat1209@gmail.com>
2018-10-01 14:39:09 +0530

    basic ui for login signup added

---
+++ aerogms/src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import { Route } from 'react-router-dom'
+// components
+import Signup from './components/signup'
+import LoginForm from './components/loginForm'
+import Dashboard from './components/dashboard'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    }
+
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  updateUser (userObject) {
+    this.setState(userObject)
+  }
+
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ')
+      console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+   
+        {/* Routes to different components */}
+        <Route
+          exact path="/dashboard"
+          component={Dashboard} />
+        <Route
+          path="/login"
+          render={() =>
+            <LoginForm
+              updateUser={this.updateUser}
+            />}
+        />
+        <Route
+          path="/signup"
+          render={() =>
+            <Signup/>}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ aerogms/src/components/dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                <p>YOu are at the dashboard</p>
+            </div>
+        )
+    }
+}
+export default Dashboard
